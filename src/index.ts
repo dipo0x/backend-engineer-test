96e0e09ba53ebea88b49da2205426ff2dfcee5fe
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2025-01-27 08:52:13 +0100

    chore: basic structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import 'dotenv/config';
+// import validateEnv from './utils/validateEnv'
+// import App from './main';
+
+// validateEnv();
+
+// const app = await new App(Number(process.env.PORT) || 3000);
+
+// app.listen();
+
+// export default app
---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import connectToDatabase from './config/database.config';
+import logger from './utils/logger.util';
+import dotenv from 'dotenv';
+dotenv.config();
+
+  const app: Application = express();
+
+  const initializeMiddleware = (): void => {
+    app.use(helmet());
+    app.use(cors());
+    app.use(morgan('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(compression());
+    app.disable('x-powered-by');
+  };
+
+  const initializeErrorHandling = (): void => {
+    app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+      logger.error(err);
+      res.status(500).send({
+        status: 500,
+        success: false,
+        message: 'Something went wrong',
+      });
+    });
+
+    app.use((req, res, next) => {
+      res.status(404).send({
+        status: 404,
+        success: false,
+        message: 'Endpoint not found',
+      });
+    });
+  };
+
+  initializeMiddleware();
+  initializeErrorHandling();
+
+  async function start () {
+    await connectToDatabase();
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on Port ${process.env.PORT}`);
+    });
+  };
+
+start()
